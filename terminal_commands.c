bb809666a497674940fa3eafd733aad5d3794bfb
ikecoo96 <32152105+ikecoo96@users.noreply.github.com>
2018-05-18 02:11:19 -0700

    Terminal Commands

---
+++ terminal_commands.c
@@ -0,0 +1,44 @@
+#include "msp.h"
+
+void move_left(int move);
+void move_down(int move);
+void move_down(int move);
+void move_topleft(void);
+void transmit_char(char data);
+
+
+void move_right(int move){
+    transmit_char(27); //ESC
+    transmit_char(91); //[
+    transmit_char(48+move); //spaces to move
+    transmit_char(67); //direction: right
+}
+
+void move_left(int move){
+    transmit_char(27); //ESC
+    transmit_char(91); //[
+    transmit_char(48+move); //spaces to move
+    transmit_char(68); //direction: left
+}
+
+void move_down(int move){
+    transmit_char(27); //ESC
+    transmit_char(91); //[
+    transmit_char(48+move); //spaces to move
+    transmit_char(66); //direction: down
+}
+
+void move_topleft(void){
+    transmit_char(27); //ESC
+    transmit_char(91); //[
+    transmit_char(72); //moves cursor to top left of terminal
+}
+
+void transmit_char(char data){
+
+    // Check if the TX buffer is empty first
+    while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
+        //send characters on the TXBUF
+        EUSCI_A0->TXBUF = data;
+}
+
---
+++ terminal_commands.h
@@ -0,0 +1,18 @@
+/*
+ * terminal_commands.h
+ *
+ *  Created on: May 17, 2018
+ *      Author: Aiku
+ */
+
+#ifndef TERMINAL_COMMANDS_H_
+#define TERMINAL_COMMANDS_H_
+
+void move_right(int move);
+void move_left(int move);
+void move_down(int move);
+void move_topleft(void);
+void transmit_char(char data);
+
+
+#endif /* TERMINAL_COMMANDS_H_ */
