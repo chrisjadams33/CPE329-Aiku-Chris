cdfbf712a5be57dd3e675eb8bc93903976405ddd
ikecoo96 <32152105+ikecoo96@users.noreply.github.com>
2018-05-18 03:00:59 -0700

    Terminal Command

--- terminal_commands.c
+++ terminal_commands.c
@@ -10,11 +10,22 @@ void move_right(int move);
 
 
 void choose_address(int horiz, int vert){
+    int count = 0;
+
+    while(horiz >= 9)
+    {
+        horiz -= 9;
+        count ++;
+    }
     transmit_char(27); //ESC
     transmit_char(91); //[
     transmit_char(48+horiz); //spaces to move cursor horizontally
-    transmit_char(58);      //spaces to move cursor vertically
+    transmit_char(59);      //spaces to move cursor vertically
     transmit_char(48+vert);
+    while(count > 0)
+    {
+        move_right(48 + 9);
+    }
     transmit_char(72); //direction: right
     }
 
